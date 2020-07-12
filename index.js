6587a2f6e633ac5c3ece74a9999dd0f4f699c6e5
github username <github email address>
2020-07-12 16:45:48 -0500

    lead changes

--- index.js
+++ index.js
@@ -8,11 +8,37 @@ document.addEventListener('DOMContentLoaded', () => {
     //console.log(newParams);
     fetchInfo(newParams);
 
-    
+    fetchLead();
 });
 
+function fetchLead(leadName) {
+    fetch("https://api.airtable.com/v0/appI40FwYNAUQHKq3/Contacts?", {
+        headers: {
+            'Authorization': 'Bearer keyemv7utChwq4g5e'
+        }
+    })
+    .then(res => res.json())
+    .then(json => {
+        for (let i in json.records) {
+            if (json.records[i]['id'] && json.records[i]['id'] === leadName) {
+                let name = json.records[i]['fields']['Name'];
+                let leadEmail = json.records[i]['fields']['Email'];
+
+                let aLead = document.createElement('a');
+                let linkTextGroup = document.createTextNode(name);
+                aLead.appendChild(linkTextGroup);
+                aLead.innerText = `${name}`;
+                aLead.href = `mailto:${leadEmail}`;
+                aLead.title = "Click Here";
+                aLead.target = "_blank";
+                document.getElementById("lead").appendChild(aLead);
+            }           
+        }
+    })
+}
+
 function fetchInfo(group) {
-    fetch("https://api.airtable.com/v0/appI40FwYNAUQHKq3/working%20groups?", {
+    fetch("https://api.airtable.com/v0/appI40FwYNAUQHKq3/tblJKfpNa4pA68rOV?", {
         headers: {
             'Authorization': 'Bearer keyemv7utChwq4g5e'
         }
@@ -33,7 +59,7 @@ function fetchInfo(group) {
                     let agendaDocument = json.records[i]['fields']['Agenda Document'];
                     let keyObjectives = json.records[i]['fields']['Objectives'];
                     //let newKey = keyObjectives.replace("/\s\d\g", "\n");
-                   
+                   console.log(leads)
                     // console.log(newKey)
                     // console.log(keyObjectives)
                     groupData.name = groupName;
@@ -43,9 +69,14 @@ function fetchInfo(group) {
                     groupData.history = groupDiscussion;
                     groupData.agenda = agendaDocument;
                     groupData.objectives = keyObjectives;
-                    //console.log(groupData.folder)
+                    console.log(groupData.folder)
                 }
             }
+            
+            const leadArray = groupData.lead.forEach(lead => fetchLead(lead));
+            //fetchLead(groupData.lead[0]);
+            //console.log(groupData.lead)
+
             let node1 = document.createElement("LI");
             let textnode1 = document.createTextNode(`${groupData.name} Working Group`);
             node1.appendChild(textnode1);
@@ -95,7 +126,7 @@ function fetchInfo(group) {
 
 
             //document.getElementById("groupName").innerText = groupData.name;
-            document.getElementById("lead").innerText = groupData.lead;
+            //document.getElementById("lead").innerText = groupData.lead;
             //document.getElementById("email").innerText = groupData.email;
             //document.getElementById("history").innerText = groupData.history;
             //document.getElementById("agenda").innerText = groupData.agenda;
