0585ff9319676c9f7fd9f12780a7f69ce868bbf0
github username <github email address>
2020-07-26 15:36:32 -0500

    added sections

---
+++ backup.js
@@ -0,0 +1,19 @@
+// function fetchInfo(group) {
+//     fetch("https://api.airtable.com/v0/appI40FwYNAUQHKq3/tblJKfpNa4pA68rOV?", {
+//         headers: {
+//             'Authorization': 'Bearer keyemv7utChwq4g5e'
+//         }
+//     })
+
+//     function fetchLead(leadName) {
+//         fetch("https://api.airtable.com/v0/appI40FwYNAUQHKq3/Contacts?", {
+//             headers: {
+//                 'Authorization': 'Bearer keyemv7utChwq4g5e'
+//             }
+//         })
+
+// function fetchLead(leadName) {
+//     fetch("https://api.airtable.com/v0/appI40FwYNAUQHKq3/Contacts?", {
+//         headers: {
+//             'Authorization': 'Bearer keyemv7utChwq4g5e'
+//         }
